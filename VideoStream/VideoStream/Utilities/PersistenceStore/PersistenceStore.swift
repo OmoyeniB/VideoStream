c70513033c23661d236010878b255abf36c76191
Omoyeni <babatundeabike3@gmail.com>
2022-11-11 01:29:49 +0100

    Create PersistenceStore.swift

---
+++ VideoStream/VideoStream/Utilities/PersistenceStore/PersistenceStore.swift
@@ -0,0 +1,31 @@
+//
+//  PersistenceStore.swift
+//  VideoStream
+//
+//  Created by Sharon Omoyeni Babatunde on 11/11/2022.
+//
+
+import Foundation
+import RealmSwift
+
+final class PersistenceStore: PersistenceProtocol {
+    
+    fileprivate let configuration = Realm.Configuration()
+    fileprivate let realm = try! Realm()
+    
+    func save<T: Object>(items: T) {
+        do {
+            try realm.write({
+                realm.add(items)
+                
+            })
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchData<T: Object>( type: T.Type) -> [T] {
+        Array(realm.objects(type))
+    }
+    
+}
