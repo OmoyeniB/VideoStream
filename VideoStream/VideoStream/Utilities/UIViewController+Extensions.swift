a5ac1dafc4102aa9ec2c022e0bba8ddec5f208a3
Omoyeni <babatundeabike3@gmail.com>
2022-11-11 01:28:42 +0100

    uiviewcontroller+extensions_to_handle_reachability_scope_and_the_navigationbar_configuration

---
+++ VideoStream/VideoStream/Utilities/UIViewController+Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  UIViewController+Extensions.swift
+//  VideoStream
+//
+//  Created by Sharon Omoyeni Babatunde on 09/11/2022.
+//
+
+
+import UIKit
+
+extension UIViewController {
+    
+    public func popUpAlert(title: String, message: String, alertStyle: UIAlertController.Style,
+                           actionTitles: [String], actionStyles: [UIAlertAction.Style],
+                           actions: [((UIAlertAction) -> Void)]) {
+        
+        let alertController = UIAlertController(title: title, message: message,
+                                                preferredStyle: alertStyle)
+        
+        for(index, indexTitle) in actionTitles.enumerated() {
+            let action = UIAlertAction(title: indexTitle,
+                                       style: actionStyles[index],
+                                       handler: actions[index])
+            
+            alertController.addAction(action)
+        }
+        
+        self.present(alertController, animated: true)
+    }
+    
+    public func displayError(error: String) {
+        
+        DispatchQueue.main.async {
+            self.popUpAlert(title: Constants.PopUpAlertString.popUpTitle,
+                            message: error,
+                            alertStyle: .alert,
+                            actionTitles: [Constants.PopUpAlertString.popUpActionTitles], actionStyles: [.default], actions: [ { _ in
+            }])
+        }
+        
+    }
+    
+    
+    func addCustomButtonInNavigationBar(button: UIButton) -> UIBarButtonItem {
+        let menuBarItem = UIBarButtonItem(customView: button)
+        menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        return menuBarItem
+    }
+
+}
