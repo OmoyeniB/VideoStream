1509654836ae6c18c036aa4001ffa9a5a159de1b
Omoyeni <babatundeabike3@gmail.com>
2022-11-11 01:15:13 +0100

    application_instatiated_from_appdelegate_and_scenedelegate

--- VideoStream/VideoStream/App/AppDelegate.swift
+++ VideoStream/VideoStream/App/AppDelegate.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
+@available(iOS 13.0, *)
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        setUpRootViewController(window: window)
         return true
     }
 
+    func setUpRootViewController(window: UIWindow) {
+        let storyboard = UIStoryboard(name: "MainViewController", bundle: nil)
+        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        let navController = UINavigationController(rootViewController: mainViewController)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- VideoStream/VideoStream/App/SceneDelegate.swift
+++ VideoStream/VideoStream/App/SceneDelegate.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let storyboard = UIStoryboard(name: "MainViewController", bundle: nil)
+        let mainViewController =  storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        let navController = UINavigationController(rootViewController: mainViewController)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
