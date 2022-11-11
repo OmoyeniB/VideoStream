775703881ca52c08e9c579b3d1a56e3d5782440f
Omoyeni <babatundeabike3@gmail.com>
2022-11-11 01:18:51 +0100

    constatbts_file_created_to_represent_strings_that_will_be_used

---
+++ VideoStream/VideoStream/Utilities/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  VideoStream
+//
+//  Created by Sharon Omoyeni Babatunde on 09/11/2022.
+//
+
+import UIKit
+
+enum Constants {
+    enum PopUpAlertString {
+        static let popUpTitle = "Alert"
+        static let popUpActionTitles = "OK"
+    }
+    enum Images {
+        static let mutedSpeaker = "muted"
+        static let unmutedSpeaker = "unmuted"
+    }
+    enum Colors {
+        static let whiteColor = UIColor(named: "WhiteColor")
+        static let blackColor = UIColor(named: "BlackColor")
+        static let greyColor = UIColor(named: "GreyColor")
+        static let lightGreyColor = UIColor(named: "LightGreyColor")
+    }
+}
